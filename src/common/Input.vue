074e8c2b5831a3dfa45128341a3bfb7071f64dc5
aleksandershurdho20 <aleksandershurdho@gmail.com>
2025-04-01 10:11:29 +0200

    feat: added login form

---
+++ src/common/Input.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineEmits, useAttrs } from 'vue';
+
+const emit = defineEmits(["update:modelValue"]);
+const attrs = useAttrs();
+</script>
+
+<template>
+  <div>
+    <label v-if="attrs.label" :for="attrs.id as string" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+      {{ attrs.label }}
+    </label>
+    <input
+      v-bind="attrs"
+      :value="attrs.modelValue"
+      @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    />
+  </div>
+</template>
--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,6 +1,54 @@
-<script lang="ts">
+<script setup lang="ts">
+import { reactive } from 'vue';
+import type { LoginData } from '@/types/Auth';
+import Input from '@/common/Input.vue';
+
+const authData = reactive<LoginData>({
+  email: '',
+  password: ''
+});
+
+const handleLogin = () => {
+  console.log(authData, "data");
+};
 </script>
 
 <template>
-    <h1>Login</h1>
+  <section class="bg-gray-50 dark:bg-gray-900 min-h-screen flex items-center justify-center">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto sm:max-w-md">
+      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white tracking-tighter">
+        <h2 class="text-blue-600 font-bold">N</h2>
+        <span>EXTO</span>
+      </a>
+      
+      <div class="w-full bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
+        <div class="p-6 space-y-6 sm:p-8">
+          <h1 class="text-xl font-bold text-gray-900 md:text-2xl dark:text-white">
+            Login to your account
+          </h1>
+          
+          <!-- Login Form -->
+          <form @submit.prevent="handleLogin" class="space-y-4">
+            <Input 
+              v-model="authData.email" 
+              id="email" 
+              type="email" 
+              label="Your email" 
+              placeholder="name@company.com"
+            />
+            <Input 
+              v-model="authData.password" 
+              id="password" 
+              type="password" 
+              label="Password" 
+              placeholder="••••••••"
+            />
+            <button type="submit" class=" cursor-pointer w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
