f85e25a288846da348dd5d653e08d021a97f9f50
aleksandershurdho20 <aleksandershurdho@gmail.com>
2025-04-11 10:35:40 +0200

    feat: added post views

--- src/views/Posts/ViewPost.vue
+++ src/views/Posts/ViewPost.vue
@@ -253,7 +253,7 @@ const toggleCommentLike = (comment) => {
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                             </svg>
-                            {{ post.views || '1.2k' }} views
+                            {{ post.post_insight.views || '1.2k' }} views
                         </span>
                         <span class="flex items-center text-gray-400 text-sm">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
